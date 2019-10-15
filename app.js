a64f5572af0c51b6766fed38d24d6419c3c8d0a8
Anthony Obando <obandoanthony.8@gmail.com>
2019-10-15 22:47:59 +0000

    EJS start

--- app.js
+++ app.js
@@ -7,12 +7,15 @@ const port = process.env.PORT || 3000;
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", 'ejs');
 
+var task = ["clean", "cook"];
+
 app.get('/', function(req, res){
     res.render("index");
 });
 
 app.post('/addtask', function(req, res){
     var newTask = req.body.newtask;
+    task.push(newTask);
     res.redirect('/');
 });
 
