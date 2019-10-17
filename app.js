6e41fac9dc99099609909b1dcccd742c21d52bae
Anthony Obando <obandoanthony.8@gmail.com>
2019-10-17 22:37:49 +0000

    Remove task

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.post('/removetask', function(req, res){
         task.splice(task.indexOf(completeTask), 1);
     }else if (typeof completeTask === "object"){
         for(var i; i < completeTask.length; i++){
-             complete.push(completeTask);
+            complete.push(completeTask);
             task.splice(task.indexOf(completeTask), 1);
         }
     }
