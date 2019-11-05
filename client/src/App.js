983d4e96704baae2d6d6cdf25f6f4f68030b3d91
Anthony Obando <obandoanthony.8@gmail.com>
2019-11-05 23:57:27 +0000

    App.js modding

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,19 @@ import {Route, Switch} from 'react-router-dom';
 import Home from './pages/Home';
 import './App.css';
 
-function App() {
-  return (
+class App extends Components {
+    render(){
+        const App = () => {
+            <div>
+                <Switch>
+                    <Route exact path = '/'component={Home} />
+                </Switch>
+            </div>
+        }
+        return (
 
-  );
+        );
+    }
 }
 
 export default App;
