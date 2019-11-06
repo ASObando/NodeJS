8e3fa9339d7616d80610e78af9be6be3ab27b04f
Anthony Obando <obandoanthony.8@gmail.com>
2019-11-06 00:08:41 +0000

    Home.js modding

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
 
 class Home extends Component{
+    //write function for fetch
     render(){
         return(
-
+            <h1>JavaScript and React suck</h1>
         );
     }
 }
