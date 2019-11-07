54f84dc3aa76cdbb384d63d8cc9527359d16765a
Anthony Obando <obandoanthony.8@gmail.com>
2019-11-07 23:33:07 +0000

    Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,18 @@
 import React, {Component} from 'react';
 
 class Home extends Component{
+    state = {
+        data: null
+    }
+
+    callBackend = async() => {
+        try{
+
+        }catch(err){
+            console.log(err);
+        }
+    }
+
     //write function for fetch
     render(){
         return(
